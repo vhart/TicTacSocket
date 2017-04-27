9434a4ed9691d256cb6637e57068fae97fa019a4
Varindra Hart <varindra@shopkeep.com>
2017-04-27 16:25:30 -0400

    initial commit

---
+++ TicTacSocket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TicTacSocket
+//
+//  Created by Varinda Hart on 4/27/17.
+//  Copyright © 2017 ShopKeep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
